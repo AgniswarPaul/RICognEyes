5ef418d86425f53b0fcb09891a6fa255d5fae536
Tamoghna-Sarkar <42618737+Tamoghna-Sarkar@users.noreply.github.com>
2019-03-31 05:26:58 -0700

    Update ocr.py

--- ocr.py
+++ ocr.py
@@ -1,14 +1,16 @@
-from PIL import Image
+from PIL import Image, ImageEnhance, ImageFilter
 import pytesseract
+path = 'image.png'
+img = Image.open(path)
+img = img.convert('RGBA')
+pix = img.load()
+for y in range(img.size[1]):
+    for x in range(img.size[0]):
+        if pix[x, y][0] < 102 or pix[x, y][1] < 102 or pix[x, y][2] < 102:
+            pix[x, y] = (0, 0, 0, 255)
+        else:
+            pix[x, y] = (255, 255, 255, 255)
+img.save('temp.jpg')
+text = pytesseract.image_to_string(Image.open('image.png'))
 
-def ocr_str(image_name):   
-    im=Image.open(image_name)
-    text=pytesseract.image_to_string(im,lang="eng")
-    return text
-
-def write():                         
-    text=open("string.txt","w+")
-    text.write(ocr_str(a))
-    text.close()
-
-ocr_str("image.png")
+print(text)
