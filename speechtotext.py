7adf9f5d9ab46611d9c770a2287ce29407c981b7
Fahad Kamraan <fahadkamraan@gmail.com>
2019-03-31 06:24:27 -0500

    Update speechtotext.py

--- speechtotext.py
+++ speechtotext.py
@@ -1,14 +1,21 @@
-import speech_recognition as sr 
+import speech_recognition as sr   
+r = sr.Recognizer() 
+  
 
-r = sr.Recognizer()
- 
-with sr.Microphone() as source:
-     print("Say something!")
-     audio = r.listen(source)
-     
-try:
-    return r.recognize_google(audio)
-except sr.UnknownValueError:
-    print("Google Speech Recognition could not understand audio")
-except sr.RequestError as e:
-    print("Could not request results from Google Speech Recognition service; {0}".format(e))
+with sr.Microphone() as source: 
+    r.adjust_for_ambient_noise(source) 
+    print( "Say Something")
+    audio = r.listen(source) 
+          
+    try: 
+        text = r.recognize_google(audio) 
+        print "you said: " + text 
+      
+    #error occurs when google could not understand what was said 
+      
+    except sr.UnknownValueError: 
+        print("Google Speech Recognition could not understand audio") 
+      
+    except sr.RequestError as e: 
+        print("Could not request results from Google  
+                                 Speech Recognition service; {0}".format(e)) 
